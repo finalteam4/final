5d2e24f5d496d5f59dc57b76103332dd44a8986e
serendipiaLia <cote9784@naver.com>
2024-02-07 18:14:15 +0900

    header footer

--- stsworks/finalProject/src/main/java/com/khit/planb/controller/MainController.java
+++ stsworks/finalProject/src/main/java/com/khit/planb/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+	
 // 로그인, 회원가입, 마이페이지 불러오기	
 	@GetMapping("/member/login")
 	public String loginForm() {
@@ -18,8 +19,13 @@ public class MainController {
 	public String accountForm() {
 		return "/member/account";
 	}
+	// footer - ADMIN : 관리자 전용 페이지(회원목록)
+	@GetMapping("/member/admin")
+	public String managementPage() {
+		return "/member/admin";
+	}
 	
-//	메뉴 불러오기
+//	header 메뉴 불러오기
 	@GetMapping("/board/preparation")
 	public String preparationPage() {
 		return "/board/preparation";
@@ -32,6 +38,23 @@ public class MainController {
 	public String communityPage() {
 		return "/board/community";
 	}
-	
-	
+	// chatbot서비스 
+	@GetMapping("/chatbot")
+	public String chatService() {
+		return "/chatbot";
+	}
+
+// footer 메뉴 (공지사항, FAQ, 개인정보처리방침)
+	@GetMapping("/board/notice")
+	public String noticePage() {
+		return "/board/notice";
+	}
+	@GetMapping("/pages/faq")
+	public String faqPage() {
+		return "/pages/faq";
+	}
+	@GetMapping("/pages/privacyPolicy")
+	public String ppPage() {
+		return "/pages/privacyPolicy";
+	}
 }
--- stsworks/finalProject/src/main/resources/static/js/favicon.js
+++ stsworks/finalProject/src/main/resources/static/js/favicon.js
@@ -12,7 +12,7 @@ function loadFavicon() {
   var link = document.createElement('link');
   link.rel = 'icon';
   link.href = '/images/favicon-96x96.png'; // favicon의 URL을 지정
-  link.type = 'image/png'; // favicon의 MIME 타입을 지정
+  link.type = 'image/png'; // favicon의 MIME 타입을 지정 , icon파일일경우 type 생략
   document.head.appendChild(link);
 }
 
