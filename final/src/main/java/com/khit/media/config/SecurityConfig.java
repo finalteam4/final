60303ff7d7faff236eb18ac4fd6f98bc9c91df2a
parkchaehee <27leechil@gmail.com>
2024-02-19 11:59:48 +0900

    -

--- final/src/main/java/com/khit/media/config/SecurityConfig.java
+++ final/src/main/java/com/khit/media/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,6 +14,8 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration    //환경 설정에 사용하는 어노테이션
 @EnableWebSecurity
 public class SecurityConfig {
+	
+
 	
 	@Autowired
 	private CustomUserDetailsService customService;
